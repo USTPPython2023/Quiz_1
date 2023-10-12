21889676bbede7f547ebafca77bf1f78ffc7eeac
Alas05 <jeremygilgenove@gmail.com>
2023-10-12 05:50:52 -0700

    First Commit

---
+++ quiz1.py
@@ -0,0 +1,11 @@
+Dieselprice = 70
+No_liter = int(input("Number of liters to buy: "))
+Total_amount_to_pay = Dieselprice * No_liter
+
+print("OUTPUT")
+print("Price of diesel:" , Dieselprice, " per liter")
+print("Number of liters: ", No_liter)
+print("Total AMOUNT TO pAY:" , Total_amount_to_pay, "Only")
+
+#
+#git config --global user.name "Your Name"
